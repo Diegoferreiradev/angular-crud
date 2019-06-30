079127e40afef47e6c6c56d5655af4930924bf5e
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-06-30 19:06:54 -0300

    Implementação do Alterar Status da Tarefa

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -29,8 +29,11 @@ export class ListarTarefaComponent implements OnInit {
   }
 
 
-  /*alterarStatus(tarefa: Tarefa): void {
-    if(confirm('Deseja alterar o status da tarefa '){}
-  }*/
+  alterarStatus(tarefa: Tarefa): void {
+    if(confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
 
 }
